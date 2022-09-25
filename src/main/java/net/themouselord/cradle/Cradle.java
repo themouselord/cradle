3016235031551a3518a3ba597561abd9798b926f
themouselord <urirayman@gmail.com>
2022-09-25 17:35:16 +0300

    Initial commit

---
+++ src/main/java/net/themouselord/cradle/Cradle.java
@@ -0,0 +1,16 @@
+package net.themouselord.cradle;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cradle implements ModInitializer {
+
+	public static final String MOD_ID = "cradle";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/themouselord/cradle/CradleClient.java
@@ -0,0 +1,10 @@
+package net.themouselord.cradle;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CradleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
